d207b5ee2b5ee0b1c0d38f15fe674481745922f7
mvill <119754846+migavg@users.noreply.github.com>
2023-01-24 17:24:53 -0700

    set my questionPool into an array, and added showQuestoin function to pull from array

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,25 @@ var questionContainer = document.getElementById("questionBlock");
 var questionElement = document.getElementById("question");
 var answerButtons = document.getElementById("answer-buttons");
 
+
+
 // these are all my classes that traverse the DOM 
 var quizTimer = document.querySelector(".timer");
 
 // a pool of questions to replace my questio id h2 tag 
-var questionPool = {
-    
-    firstQuestion:"Commonly used data types DO NOT include:",
-    secondQuestion:"What is 1 + 1?",
-    secondQuestion:"What is 1 + 1?",
-    secondQuestion:"What is 1 + 1?"
+var questionPool =
+    [
+        {
+            firstQuestion: "Commonly used data types DO NOT include:",
+            firstAnswer: [
+                { text: "alerts", correct: true },
+                { text: "booleans", correct: false },
+                { text: "strings", correct: false },
+                { text: "numbers", correct: false },
 
-
-}
-
+            ]
+        }
+    ]
 
 
 // timer function
@@ -33,21 +38,45 @@ function startQuiz() {
         }
     }, 1000);
 
-startButton.classList.add("hide");
-startPage.classList.add("hide");
-questionContainer.classList.remove("hide");
-showQuestion();
+    startButton.classList.add("hide");
+    startPage.classList.add("hide");
+    questionContainer.classList.remove("hide");
+    showQuestion();
 }
 
 
 // shows the first question in the quiz 
 function showQuestion() {
-    questionElement.innerText = questionPool.firstQuestion;
-
-
+    questionElement.innerText = questionPool[0].firstQuestion;
+    questionPool[0].firstAnswer.forEach(function(answer) {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtons.appendChild(button)
+    })
 }
 
 
+
+
+
+function resetState() {
+while (answerButtons.firstChild) {
+    answerButtons.removeChild
+    (answerButtons.firstChild)
+}
+
+}
+
+function selectAnswer(event) {
+    resetState();
+ 
+
+}
 
 
 // button that is suppose to start the quiz 
